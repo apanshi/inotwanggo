3e205112a8f8397ae5472791e7746d32ba286ff8
panshi <shix_112@126.com>
2022-03-01 19:36:00 +0800

    more notes for go

--- main.go
+++ main.go
@@ -9,6 +9,18 @@ import (
 	"main/internal/tforgo"
 )
 
+func init() {
+	fmt.Println("Go 初始化 int 1: ", a)
+}
+
+func init() {
+	fmt.Println("Go 初始化 int 2: ", b)
+}
+
+var a = 10
+
+const b = 100
+
 func OtherMain() {
 	fmt.Printf("Go 第一个程序\n")
 	stgo.Smain()
@@ -18,8 +30,10 @@ func OtherMain() {
 }
 
 func main() {
-	fmt.Println("Go 第三方库")
+	defer fmt.Println("Go 第三方库, defer: ", quote.Hello())
 	fmt.Println(quote.Hello())
 
 	OtherMain()
+
+	fmt.Println("Go 初始化 main(): ", b)
 }
